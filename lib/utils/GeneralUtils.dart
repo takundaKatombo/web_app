df14699f38524a94e84b47f8c0cc4295438036d8
pbryson <43042495+pbryson@users.noreply.github.com>
2021-02-08 23:55:57 +0200

    Create GeneralUtils.dart

---
+++ lib/utils/GeneralUtils.dart
@@ -0,0 +1,23 @@
+//import 'package:web_app/src/data/ConfigData.dart';
+
+bool isThisAuthorized(String methodToCall)
+{
+  return false ; // Show component ie do NOT move OffStage
+
+  /*
+  if (ConfigData().adminUser) return true ;
+  if (ConfigData().RBAMap.containsKey(methodToCall))
+  {
+    // var dict:Array = config.authorizationDict[methodToCall] as Array;
+    for (int i=0; i< ConfigData().RBAMap.length ; i++)
+    {
+      if (ConfigData().roles.contains(ConfigData().RBAMap[i]))
+      {
+        return false ; // Show component ie do NOT move OffStage
+      }
+    }
+  }
+  return true ; // Dont show component ie DO move OffStage
+
+   */
+}
