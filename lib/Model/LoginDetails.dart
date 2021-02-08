f4af9efc5bce960edb064e07050f559585a55e60
pbryson <43042495+pbryson@users.noreply.github.com>
2021-02-08 18:01:21 +0200

    Create LoginDetails.dart

---
+++ lib/Model/LoginDetails.dart
@@ -0,0 +1,58 @@
+class LoginDetails {
+
+  int userID ;
+  String userName ;
+  String firstName ;
+  String lastName ;
+  String password ;
+  String currentState ; //'loggedOut'
+  String loginStatus ;
+  String resultDescription ;
+  String defaultRole ;
+  String clientIPAddress ;
+  String remoteIp ; //= "IpNotSet."
+  List<String> roles ;
+
+
+
+  LoginDetails({
+    this.userID = -1, this.userName , this.firstName, this.lastName,
+    this.password = '', this.currentState = '', this.loginStatus = '', this.resultDescription = '',
+    this.defaultRole = '', this.clientIPAddress = '', this.remoteIp = '', this.roles
+  }
+      ) ;
+
+
+  Map<String, dynamic> toJson() =>
+      {
+        'userID': userID,
+        'userName': userName,
+        'firstName': firstName,
+        'lastName': lastName,
+        'password': password,
+        'currentState': currentState,
+        'loginStatus': loginStatus,
+        'resultDescription': resultDescription,
+        'defaultRole': defaultRole,
+        'clientIPAddress': clientIPAddress,
+        'remoteIp': remoteIp,
+        'roles': roles
+      };
+
+  factory LoginDetails.fromJson(Map<String, dynamic> json) {
+    return LoginDetails(
+        userID: json['userID'],
+        userName: json['userName'],
+        firstName: json['firstName'],
+        lastName: json['lastName'],
+        password: json['password'],
+        currentState: json['currentState'],
+        loginStatus: json['loginStatus'],
+        resultDescription: json['resultDescription'],
+        defaultRole: json['defaultRole'],
+        clientIPAddress: json['clientIPAddress'],
+        remoteIp: json['remoteIp'],
+        roles: json['roles']
+    );
+  }
+}
