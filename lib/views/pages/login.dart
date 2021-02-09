f7d936572e10e4c3868d0f7ac449075a23baea45
takundaKatombo <70145501+takundaKatombo@users.noreply.github.com>
2021-02-09 17:56:20 +0200

    material app on login

--- lib/views/pages/login.dart
+++ lib/views/pages/login.dart
@@ -8,19 +8,21 @@ class Login extends StatefulWidget{
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      
-      child:Column(
-      children: [Container(height: 50),
-          Container(width:MediaQuery.of(context).size.width*0.4,height: MediaQuery.of(context).size.height*0.1,child: TextField(decoration: const InputDecoration(hintText:'Username',labelText:'Username'),)),
-          Container(width:MediaQuery.of(context).size.width*0.4,height: MediaQuery.of(context).size.height*0.1,child: TextField(decoration: const InputDecoration(hintText:'Password',labelText:'Password'),)),
-          ElevatedButton(
-            onPressed: () { loginCall(1)?Navigator.pushNamed(context, '/home'):showAlertDialog(context);},
-            child: Text('Login'),
-            ),Container(height: 10),ElevatedButton(
-            onPressed: () { loginCall(0)?Navigator.pushNamed(context, '/home'):showAlertDialog(context);},
-            child: Text('Login Failed'),
-            )]));
+    return MaterialApp(
+          home: Center(
+        
+        child:Column(
+        children: [Container(height: 50),
+            Container(width:MediaQuery.of(context).size.width*0.4,height: MediaQuery.of(context).size.height*0.1,child: TextField(decoration: const InputDecoration(hintText:'Username',labelText:'Username'),)),
+            Container(width:MediaQuery.of(context).size.width*0.4,height: MediaQuery.of(context).size.height*0.1,child: TextField(decoration: const InputDecoration(hintText:'Password',labelText:'Password'),)),
+            ElevatedButton(
+              onPressed: () { loginCall(1)?Navigator.pushNamed(context, '/home'):showAlertDialog(context);},
+              child: Text('Login'),
+              ),Container(height: 10),ElevatedButton(
+              onPressed: () { loginCall(0)?Navigator.pushNamed(context, '/home'):showAlertDialog(context);},
+              child: Text('Login Failed'),
+              )])),
+    );
   }
 
   bool loginCall(int val){if(val==1)return true; else return false;}
