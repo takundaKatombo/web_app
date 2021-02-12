14ff157576d2a06162183142b402b04ffaeb1d79
takundaKatombo <mtkatombo@gmail.com>
2021-02-12 09:00:40 +0200

    code refactoring

--- lib/views/pages/homepage.dart
+++ lib/views/pages/homepage.dart
@@ -6,11 +6,8 @@ import 'package:web_app/views/pages/cable_drums/cabledrums.dart';
 import 'package:web_app/views/pages/sqlpage.dart';
 import 'package:web_app/views/widgets/tabview.dart';
 
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  
 
   final String title;
 
@@ -20,167 +17,264 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<String> data = ['Welcome Page'];
-    int initPosition = 0 ; String dropdownValue = 'One';
-  var _selectedtab=0;
-  
+  int initPosition = 0;
+  String dropdownValue = 'One';
+
+  int _selectedtab;
   @override
   Widget build(BuildContext context) {
     var litems = Provider.of<BottomNotifications>(context);
     return Scaffold(
       appBar: AppBar(
-automaticallyImplyLeading: false,
-        title: Row(children: [Offstage( offstage: isThisAuthorized('theTaskAction'),
-          child: PopupMenuButton(child: Text('Projects'),
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            Offstage(
+              offstage: isThisAuthorized('theTaskAction'),
+              child: PopupMenuButton(
+                child: Text('Projects'),
+                itemBuilder: (BuildContext bc) => [
+                  PopupMenuItem(
+                    enabled: !isThisAuthorized('theTaskAction'),
+                    child: Text("Syncable SQL"),
+                    value: 'sqlpage',
+                  ),
+                  PopupMenuItem(
+                    child: Text("Item 2"),
+                  ),
+                  PopupMenuItem(
+                    child: Text("Item 3"),
+                  ),
+                ],
+                onSelected: (value) {
+                  if (value == 'sqlpage')
+                    setState(() {
+                      if (!data.contains('Syncable SQL')) {
+                        data.add('Syncable SQL');
+                        _selectedtab = 1;
+                      }
+                    });
+                },
+              ),
+            ),
+            Container(
+              width: 10,
+            ),
+            PopupMenuButton(
+              child: Text('Staff'),
               itemBuilder: (BuildContext bc) => [
-                PopupMenuItem( enabled: ! isThisAuthorized('theTaskAction') ,child: Text("Syncable SQL"),value: 'sqlpage',),
-                PopupMenuItem(child: Text("Item 2"), ),
-                PopupMenuItem(child: Text("Item 3"), ),
+                PopupMenuItem(child: Text("Item 1")),
+                PopupMenuItem(
+                  child: Text("Item 2"),
+                ),
+                PopupMenuItem(
+                  child: Text("Item 3"),
+                ),
+              ],
+              onSelected: (route) {},
+            ),
+            Container(
+              width: 10,
+            ),
+            PopupMenuButton(
+              child: Text('Stock Control'),
+              itemBuilder: (BuildContext bc) => [
+                PopupMenuItem(child: Text("Item 1")),
+                PopupMenuItem(
+                  child: Text("Item 2"),
+                ),
+                PopupMenuItem(
+                  child: Text("Item 3"),
+                ),
+              ],
+              onSelected: (route) {},
+            ),
+            Container(
+              width: 10,
+            ),
+            PopupMenuButton(
+              child: Text('Cable Drums'),
+              itemBuilder: (BuildContext bc) => [
+                PopupMenuItem(
+                  enabled: !isThisAuthorized('theTaskAction'),
+                  child: Text("Cable Drums"),
+                  value: 'cabledrums',
+                ),
+                PopupMenuItem(
+                  child: Text("Item 2"),
+                ),
+                PopupMenuItem(
+                  child: Text("Item 3"),
+                ),
               ],
               onSelected: (value) {
-                 if(value=='sqlpage')setState(() {
-                if(!data.contains('Syncable SQL')){data.add('Syncable SQL');_selectedtab = 1;}
-              });
+                if (value == 'cabledrums')
+                  setState(() {
+                    if (!data.contains('Cable Drums')) {
+                      data.add('Cable Drums');
+                      _selectedtab = 1;
+                    }
+                  });
               },
             ),
-        ),Container(width: 10,),PopupMenuButton(child: Text('Staff'),
-            itemBuilder: (BuildContext bc) => [
-              PopupMenuItem(child: Text("Item 1")),
-              PopupMenuItem(child: Text("Item 2"), ),
-              PopupMenuItem(child: Text("Item 3"), ),
-            ],
-            onSelected: (route) {
-              
-            },
-          ),Container(width: 10,),PopupMenuButton(child: Text('Stock Control'),
-            itemBuilder: (BuildContext bc) => [
-              PopupMenuItem(child: Text("Item 1")),
-              PopupMenuItem(child: Text("Item 2"), ),
-              PopupMenuItem(child: Text("Item 3"), ),
-            ],
-            onSelected: (route) {
-              
-            },
-          ),Container(width: 10,),PopupMenuButton(child: Text('Cable Drums'),
-            itemBuilder: (BuildContext bc) => [
-                PopupMenuItem( enabled: ! isThisAuthorized('theTaskAction') ,child: Text("Cable Drums"),value: 'cabledrums',),
-              PopupMenuItem(child: Text("Item 2"), ),
-              PopupMenuItem(child: Text("Item 3"), ),
-            ],
-           onSelected: (value) {
-                 if(value=='cabledrums')setState(() {
-                if(!data.contains('Cable Drums')){data.add('Cable Drums');_selectedtab = 1;}
-              });
-              },
-          ),Container(width: 10,),PopupMenuButton(child: Text('Cable Process'),
-            itemBuilder: (BuildContext bc) => [
-              PopupMenuItem(child: Text("Item 1")),
-              PopupMenuItem(child: Text("Item 2"), ),
-              PopupMenuItem(child: Text("Item 3"), ),
-            ],
-            onSelected: (route) {
-              
-            },
-          ),Container(width: 10,),PopupMenuButton(child: Text('Site Administration'),
-            itemBuilder: (BuildContext bc) => [
-              PopupMenuItem(child: Text("Item 1"),),
-              PopupMenuItem(child: Text("Item 2"),),
-              PopupMenuItem(child: Text("Item 3"),),
-            ],
-            onSelected: (route) {
-              
-            },
-          ),Container(width: 10,),PopupMenuButton(child: Text('CMS Administration'),
-            itemBuilder: (BuildContext bc) => [
-              PopupMenuItem(child: Text("Item 1"),),
-              PopupMenuItem(child: Text("Item 2"),),
-              PopupMenuItem(child: Text("Item 3"),),
-            ],
-            onSelected: (route) {
-              
-            },
-          ),Container(width: 10,), PopupMenuButton(child: Text('Help'),
-            itemBuilder: (BuildContext bc) => [
-              PopupMenuItem(child: Text("Item 1"), ),
-              PopupMenuItem(child: Text("Item 2"), ),
-              PopupMenuItem(child: Text("Item 3"), ),
-            ],
-            onSelected: (route) {
-              
-            },
-          )],),
+            Container(
+              width: 10,
+            ),
+            PopupMenuButton(
+              child: Text('Cable Process'),
+              itemBuilder: (BuildContext bc) => [
+                PopupMenuItem(child: Text("Item 1")),
+                PopupMenuItem(
+                  child: Text("Item 2"),
+                ),
+                PopupMenuItem(
+                  child: Text("Item 3"),
+                ),
+              ],
+              onSelected: (route) {},
+            ),
+            Container(
+              width: 10,
+            ),
+            PopupMenuButton(
+              child: Text('Site Administration'),
+              itemBuilder: (BuildContext bc) => [
+                PopupMenuItem(
+                  child: Text("Item 1"),
+                ),
+                PopupMenuItem(
+                  child: Text("Item 2"),
+                ),
+                PopupMenuItem(
+                  child: Text("Item 3"),
+                ),
+              ],
+              onSelected: (route) {},
+            ),
+            Container(
+              width: 10,
+            ),
+            PopupMenuButton(
+              child: Text('CMS Administration'),
+              itemBuilder: (BuildContext bc) => [
+                PopupMenuItem(
+                  child: Text("Item 1"),
+                ),
+                PopupMenuItem(
+                  child: Text("Item 2"),
+                ),
+                PopupMenuItem(
+                  child: Text("Item 3"),
+                ),
+              ],
+              onSelected: (route) {},
+            ),
+            Container(
+              width: 10,
+            ),
+            PopupMenuButton(
+              child: Text('Help'),
+              itemBuilder: (BuildContext bc) => [
+                PopupMenuItem(
+                  child: Text("Item 1"),
+                ),
+                PopupMenuItem(
+                  child: Text("Item 2"),
+                ),
+                PopupMenuItem(
+                  child: Text("Item 3"),
+                ),
+              ],
+              onSelected: (route) {},
+            )
+          ],
+        ),
       ),
-  //     persistentFooterButtons: <Widget>[
-  // Icon(Icons.email),
-  // Icon(Icons.remove)
+      //     persistentFooterButtons: <Widget>[
+      // Icon(Icons.email),
+      // Icon(Icons.remove)
 // ],
 
- body:
-     Container(decoration: BoxDecoration(
-      border: Border.all(
-        color: Colors.blue,
+      body: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.blue,
+          ),
+          // borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: CustomTabView(
+          initPosition: initPosition,
+          itemCount: data.length,
+          tabBuilder: (context, index) => Tab(
+              child: GestureDetector(
+            child: Text(data[index]),
+            onDoubleTap: () {
+              setState(() {
+                data.remove(data[index]);
+              });
+            },
+          )),
+          pageBuilder: (context, index) => selectPage(
+              data[index]), //function call to select tab depending on index
+          onPositionChange: (index) {
+            setState(() {
+              index == 0 ? _selectedtab = 0 : _selectedtab = 1;
+            });
+            initPosition = index;
+          },
+          onScroll: (position) => print('$position'),
+        ),
       ),
-      // borderRadius: BorderRadius.circular(10.0),
-    ),
-       child: CustomTabView( 
-        initPosition: initPosition,
-        itemCount: data.length,
-        tabBuilder: (context, index) => Tab(child: GestureDetector(child: Text( data[index]),onDoubleTap: (){setState(() {
-          data.remove( data[index]);
-        });},)),
-        pageBuilder: (context, index) => selectPage(data[index]),//function call to select tab depending on index 
-        onPositionChange: (index){
-         setState(() {
-           index==0?_selectedtab=0:_selectedtab=1;
-         });
-          initPosition = index;
-        },
-        onScroll: (position) => print('$position'),
- ),
-     ),
 
-     bottomNavigationBar: Container(decoration: BoxDecoration(
-      border: Border.all(
-        color: Colors.blue,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.blue,
+          ),
+          // borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue,
+                ),
+                // borderRadius: BorderRadius.circular(10.0),
+              ),
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: new ListView.builder(
+                  itemCount: litems.litems.length,
+                  itemBuilder: (BuildContext ctxt, int index) {
+                    return new Text(litems.litems[index]);
+                  }),
+            ),
+            Spacer(),
+            ElevatedButton(
+                onPressed: () {
+                  litems.clear();
+                },
+                child: Text('Clear')),
+            Spacer(),
+          ],
+        ),
       ),
-      // borderRadius: BorderRadius.circular(10.0),
-    ),
-       child: Row(
-         children: [
-           Container(decoration: BoxDecoration(
-      border: Border.all(
-        color: Colors.blue,
-      ),
-      // borderRadius: BorderRadius.circular(10.0),
-    ), height: MediaQuery.of(context).size.height*0.1,width:MediaQuery.of(context).size.width*0.8,child: new ListView.builder
-                  
-                    (
-                  
-                      itemCount: litems.litems.length,
-                  
-                      itemBuilder: (BuildContext ctxt, int index) {
-                  
-                       return new Text(litems.litems[index]);
-                  
-                      }
-                  
-                    ),),Spacer(),ElevatedButton(onPressed:  (){litems.clear();}, child: Text('Clear')),Spacer(),
-         ],
-       ),
-     ),
-          
     );
   }
 
-  Widget selectPage(String pageName)
-  {
+  Widget selectPage(String pageName) {
     switch (pageName) {
-      case 'Cable Drums':return CableDrums();break;
-
-      case 'Syncable SQL':return SQLPage();
-        
+      case 'Cable Drums':
+        return CableDrums();
         break;
-      default:return Container();
+
+      case 'Syncable SQL':
+        return SQLPage();
+
+        break;
+      default:
+        return Container();
     }
   }
 }
-
--- lib/views/pages/sqlpage.dart
+++ lib/views/pages/sqlpage.dart
@@ -15,50 +15,93 @@ class _SQLPageState extends State<SQLPage> {
   var val = 1;
 
   @override
-  Widget build(BuildContext context) {   
-   var litems = Provider.of<BottomNotifications>(context);
+  Widget build(BuildContext context) {
+    var litems = Provider.of<BottomNotifications>(context);
 
-    return Column(children: [Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Center(child: Text('Please Enter The Sql Below')),
-    ),Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Center(child: Text('Tips')),
-    ),Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Center(child: Text('Text Here')),
-    ),Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Center(child: Text('Examples ')),
-    ),Padding(
-      padding: const EdgeInsets.all(8.0),
-      child:Center(child: Row(children: [Text('General Update'),Container(width:MediaQuery.of(context).size.width*0.7,child: Text('Sample Query'),decoration: BoxDecoration(
-      border: Border.all(
-        color: Colors.blue,
-      ),
-      // borderRadius: BorderRadius.circular(10.0),
-    ),)],)),
-    ),Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Center(child: Row(children: [Text('General Update'),Container(width:MediaQuery.of(context).size.width*0.7,
-        child: Text('Sample Text '),decoration: BoxDecoration(
-      border: Border.all(
-        color: Colors.blue,
-      ),
-      // borderRadius: BorderRadius.circular(10.0),
-    ),
-      ),],),),
-    ),Padding(
-      padding: const EdgeInsets.all(8.0),
-      child:Center(child: Container(child:Text('Results Here'),height:MediaQuery.of(context).size.height*0.2,width:MediaQuery.of(context).size.width*0.8,decoration: BoxDecoration(
-      border: Border.all(
-        color: Colors.blue,
-      ),
-      // borderRadius: BorderRadius.circular(10.0),
-    ),)),
-    ),Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(onPressed: (){litems.addNotification('Execute pressed $val');val++;}, child: Text('Execute')),
-    ),Spacer(),Container()],);
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(child: Text('Please Enter The Sql Below')),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(child: Text('Tips')),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(child: Text('Text Here')),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(child: Text('Examples ')),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+              child: Row(
+            children: [
+              Text('General Update'),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: Text('Sample Query'),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue,
+                  ),
+                  // borderRadius: BorderRadius.circular(10.0),
+                ),
+              )
+            ],
+          )),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Row(
+              children: [
+                Text('General Update'),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: Text('Sample Text '),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.blue,
+                    ),
+                    // borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+              child: Container(
+            child: Text('Results Here'),
+            height: MediaQuery.of(context).size.height * 0.2,
+            width: MediaQuery.of(context).size.width * 0.8,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blue,
+              ),
+              // borderRadius: BorderRadius.circular(10.0),
+            ),
+          )),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+              onPressed: () {
+                litems.addNotification('Execute pressed $val');
+                val++;
+              },
+              child: Text('Execute')),
+        ),
+        Spacer(),
+        Container()
+      ],
+    );
   }
 }
