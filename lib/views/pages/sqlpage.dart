7aab950a1affff8f19d2bab9992f0c8054bdad62
pbryson <43042495+pbryson@users.noreply.github.com>
2021-02-13 13:02:29 +0200

    Paul - SQLPage

--- lib/views/pages/sqlpage.dart
+++ lib/views/pages/sqlpage.dart
@@ -1,107 +1,84 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:web_app/controllers/bottomnotifications.dart';
 
-class SQLPage extends StatefulWidget {
+
+// Page vars
+String sqlUpdateText ;
+
+class SQLPage extends StatelessWidget {
   const SQLPage({
     Key key,
   }) : super(key: key);
 
-  @override
-  _SQLPageState createState() => _SQLPageState();
-}
+  SQLPage.forDesignTime();
 
-class _SQLPageState extends State<SQLPage> {
-  var val = 1;
 
   @override
   Widget build(BuildContext context) {
     var litems = Provider.of<BottomNotifications>(context);
 
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(child: Text('Please Enter The Sql Below')),
+    return Column( children:
+    [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Align( alignment: Alignment.topLeft, child: Text("Example 1 :  ", style: TextStyle(fontWeight: FontWeight.bold),)),
+            Align( alignment: Alignment.topLeft, child: SizedBox(width: MediaQuery.of(context).size.width*0.8 ,child: Text("update cable_drums set manu_batch_num = '123'  where  drum_id='_1404900531742'", overflow: TextOverflow.visible ))),
+          ],
         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(child: Text('Tips')),
+      ), //End child 1
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Align( alignment: Alignment.topLeft, child: Text( "Example 2 :  ", style: TextStyle(fontWeight: FontWeight.bold),)),
+            Align(alignment: Alignment.topLeft, child: SizedBox(width: MediaQuery.of(context).size.width*0.8 ,child: Text("update stock_bins_history set trans_date = to_timestamp('2004/11/29 14:30:15', 'YYYY/MM/DD HH24:MI:SS')  where trans_hist_id = '1_1404900531600'", overflow: TextOverflow.visible))),
+          ],
         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(child: Text('Text Here')),
+      ), //End child 2
+
+
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child:Center(child:
+        Container( height:MediaQuery.of(context).size.height*0.2,
+          decoration: BoxDecoration(border: Border.all(color: Colors.blue,),
+            borderRadius: BorderRadius.circular(10.0),),
+          child: TextField( keyboardType: TextInputType.multiline, maxLines: null ,onChanged: (text) {sqlUpdateText = text; },
+              decoration: InputDecoration( hintText: 'Enter your sql here')),
         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(child: Text('Examples ')),
         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-              child: Row(
-            children: [
-              Text('General Update'),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.7,
-                child: Text('Sample Query'),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.blue,
-                  ),
-                  // borderRadius: BorderRadius.circular(10.0),
-                ),
-              )
-            ],
-          )),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-            child: Row(
-              children: [
-                Text('General Update'),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.7,
-                  child: Text('Sample Text '),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.blue,
-                    ),
-                    // borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-              child: Container(
-            child: Text('Results Here'),
-            height: MediaQuery.of(context).size.height * 0.2,
-            width: MediaQuery.of(context).size.width * 0.8,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.blue,
-              ),
-              // borderRadius: BorderRadius.circular(10.0),
-            ),
-          )),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ElevatedButton(
-              onPressed: () {
-                litems.addNotification('Execute pressed $val');
-                val++;
-              },
-              child: Text('Execute')),
-        ),
-        Spacer(),
-        Container()
-      ],
-    );
+
+      ),Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: RaisedButton( elevation: 4.0, splashColor: Colors.yellow,onPressed: executeJob, child: Text('Execute')),
+    )],);
   }
+}
+
+Future<String> executeJob() async
+{
+
+  List<dynamic> entityElements = [sqlUpdateText];
+//  List resultElements = await getElementsFromServer(isPost: true, entityElements: entityElements, endPoint: ConfigData().siteControlURL, methodToCall: Constants.METHOD_GENERIC_SYNC_SQL);
+
+
+  List<dynamic> resultElements = ['testValue'];
+
+  if (resultElements != null)
+  {
+    for (int i=0 ; i< resultElements.length ; i++)
+    {
+      //Todo Populate some field if required
+      //litems.addNotification('Execute pressed ') ;
+    }
+    //
+  }
+
+//  print(sqlUpdateText);
+
 }
